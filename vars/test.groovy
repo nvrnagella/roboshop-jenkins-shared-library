1b2c911747c2f926e7e236857b827c4f359983cc
IT1089778 <nvrnagella@gmail.com>
2023-03-31 11:32:38 +0530

    declaring one more script inside same step

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,7 +1,9 @@
-def abc = "Hello"
-def xyz = 10
+def test1(){
+    def abc = "Hello"
+    def xyz = 10
 
-print "abc = ${abc}"
-print "xyz = ${xyz}"
+    print "abc = ${abc}"
+    print "xyz = ${xyz}"
 
-print abc
+    print abc
+}
