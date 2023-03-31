3c78e2f0cc9a381c6be6710f1856706344f10feb
IT1089778 <nvrnagella@gmail.com>
2023-03-31 22:53:31 +0530

    dispatch, payment added jenkinsfile

--- vars/common.groovy
+++ vars/common.groovy
@@ -5,4 +5,9 @@ def compile(){
     if ( app_lang == "maven"){
         sh 'mvn package'
     }
+    if ( app_lang == "golang"){
+        sh 'go mod init dispatch'
+        sh 'go get'
+        sh 'go build'
+    }
 }
