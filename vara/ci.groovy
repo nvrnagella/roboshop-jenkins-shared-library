7da2a0633c223cf65338dc4c9f73a80dba17f346
IT1089778 <nvrnagella@gmail.com>
2023-03-31 08:05:59 +0530

    for keeping code dry added jenkins shared library

---
+++ vara/ci.groovy
@@ -0,0 +1,30 @@
+
+def call(){
+    pipeline{
+        agent {
+            label 'workstation'
+        }
+        stages{
+            stage('compile/built'){
+                steps{
+                    echo 'compile'
+                }
+            }
+            stage('unit tests'){
+                steps{
+                    echo 'unit tests'
+                }
+            }
+            stage('quality control'){
+                steps{
+                    echo 'quality control'
+                }
+            }
+            stage('upload to centralized place'){
+                steps{
+                    echo 'upload'
+                }
+            }
+        }
+    }
+}
