0ab3d43dfab359c6dac77dc1eee65fc3d5932ce8
IT1089778 <nvrnagella@gmail.com>
2023-03-31 12:31:34 +0530

    env variable declared in jenkinsfile under catalogue

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,4 +1,5 @@
 def new1(){
+    //declaring variable and accessing
     print "Hello"
     def xyz = 10
 
@@ -6,21 +7,29 @@ def new1(){
     print "xyz = ${xyz}"
 
     print abc
+
+    //conditions
     if ( abc == "some data") {
         print "YES"
     }
         else{
             print "NO"
         }
+
+    //while loop
     x = 10
     y = 0
     while(x>y){
         println "${y}"
         y++
     }
+
+    //for loop
     for (i=0;i<5;i++){
         println(i)
     }
+
+    //for in loop
     def fruits = ["apple","banana","mango"];
     for (i in fruits){
         println(i)
