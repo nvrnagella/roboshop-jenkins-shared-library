04e52c4be7be87251b137b60881ff75f566154dc
IT1089778 <nvrnagella@gmail.com>
2023-04-01 08:21:35 +0530

    maven unit test cases lets try

--- vars/common.groovy
+++ vars/common.groovy
@@ -12,12 +12,13 @@ def compile(){
     }
 }
 def unittests(){
+    //developer is missing unittests cases in this project as a best practice he needs to add them
     if ( app_lang == "nodejs"){
         //sh 'npm test'
         sh 'echo Test cases'
     }
     if ( app_lang == "maven"){
-        sh 'mvn package'
+        sh 'mvn test'
     }
     if ( app_lang == "golang"){
         sh 'go test'
