47994f4abec43f49dc27785dc63b264ee2d610b7
IT1089778 <nvrnagella@gmail.com>
2023-04-02 05:32:11 +0530

    we have wrapped sonar qube passwords to be not display on jobs output

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -23,10 +23,15 @@ def call(){
             stage('quality control'){
                 environment{
                    SONAR_USER= '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
-                    SONAR_PASS= '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                    //SONAR_PASS= '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
                 }
                 steps{
-                    sh "sonar-scanner -Dsonar.host.url=http://172.31.6.251:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
+                    SONAR_PASS= sh (script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                    wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]){
+                        println "Password = ${SONAR_PASS}"
+                        sh "echo sh password = ${SONAR_PASS}"
+                        sh "sonar-scanner -Dsonar.host.url=http://172.31.6.251:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
+                }
                 }
             }
             stage('upload to centralized place'){
