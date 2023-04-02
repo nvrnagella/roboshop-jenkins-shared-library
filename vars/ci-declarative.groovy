6b21aa602377c41679f06176818e4bd6cc86dca1
IT1089778 <nvrnagella@gmail.com>
2023-04-02 09:05:22 +0530

    added checkout stage

--- vars/ci-declarative.groovy
+++ vars/ci-declarative.groovy
@@ -1,23 +1,27 @@
 def call(){
     try{
        node('workstation') {
-            stage('compile/built') {
-                common.compile()
-            }
-            stage('unit tests') {
-                common.unittests()
-            }
-            stage('quality control') {
-                SONAR_PASS = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-                SONAR_PASS = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-                wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
-                    sh "sonar-scanner -Dsonar.host.url=http://172.31.6.251:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
-                }
-            }
-            stage('upload to centralized place') {
-                echo 'upload'
-            }
-        }
+           stage('Checkout') {
+               cleanWs()
+               git branch: 'main', url: 'https://github.com/nvrnagella/cart'
+           }
+           stage('compile/built') {
+               common.compile()
+           }
+           stage('unit tests') {
+               common.unittests()
+           }
+           stage('quality control') {
+               SONAR_PASS = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+               SONAR_PASS = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+               wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
+                   sh "sonar-scanner -Dsonar.host.url=http://172.31.6.251:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
+               }
+           }
+           stage('upload to centralized place') {
+               echo 'upload'
+           }
+       }
     }catch (Exception e){
         common.email("failed")
     }
