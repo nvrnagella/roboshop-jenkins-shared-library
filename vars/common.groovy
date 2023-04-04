3416033e3c214b267ee5475c2ee69a63703c3987
IT1089778 <nvrnagella@gmail.com>
2023-04-04 14:57:59 +0530

    shipping tag created

--- vars/common.groovy
+++ vars/common.groovy
@@ -42,7 +42,7 @@ def artifactPush(){
         sh "zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION ${extraFiles}"
     }
     if (app_lang == "golang") {
-        sh "zip -r ${component}-${TAG_NAME}.zip -x Jenkinsfile VERSION"
+        sh "zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile"
     }
 
     NEXUS_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
