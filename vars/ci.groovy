91de79057d2394495ddf66b3b6d7817f074f9008
IT1089778 <nvrnagella@gmail.com>
2023-04-04 12:34:54 +0530

    code pushing to nexus

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,13 +1,13 @@
 def call() {
     if (!env.SONAR_EXTRA_OPTS) {
         env.SONAR_EXTRA_OPTS = ""
-    }
+
 
     if (!env.TAG_NAME) {
         env.PUSH_CODE = "false"
     } else {
         env.PUSH_CODE = "true"
-    }
+    }}
 
     try {
         node('workstation') {
